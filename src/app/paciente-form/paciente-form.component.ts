0b0595bf4dfa403c8cf8589ac57a9fb64f0c3014
CynthiaOrtiz <cynthia.ortiz.ceol@gmail.com>
2021-02-18 18:26:29 -0500

    2 files modified

--- src/app/paciente-form/paciente-form.component.ts
+++ src/app/paciente-form/paciente-form.component.ts
@@ -1,19 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 
 import { Paciente } from "../paciente";
 import { ActivatedRoute } from "@angular/router";
-
+import { NgForm } from "@angular/forms";
 @Component({
   selector: "app-paciente-form",
   templateUrl: "./paciente-form.component.html",
   styleUrls: ["./paciente-form.component.css"]
 })
 export class PacienteFormComponent {
-  model = new Paciente(17, "Cya", "Pz", "dir", 915);
+  paciente = new Paciente(0, "", "", "", 0);
 
+  @ViewChild("pacienteForm", null) pacienteForm: NgForm;
   constructor(private _route: ActivatedRoute) {}
 
   onSubmit() {
+    console.log("SUBMIT");
     this.newHero();
 
     //ir a la pagina de busqueda de paciente
@@ -21,13 +23,17 @@ export class PacienteFormComponent {
 
   // TODO: Remove this when we're done
   get diagnostic() {
-    return JSON.stringify(this.model);
+    return JSON.stringify(this.paciente);
   }
 
   newHero() {
-    this.model = new Paciente(0, "", "", "", 0);
+    this.paciente = new Paciente(0, "", "", "", 0);
   }
-
+  guardarPaciente() {
+    console.log("guardar PAciente", this.paciente);
+    this.newHero();
+    this.pacienteForm.reset();
+  }
   /////////////////////////////
 }
 
