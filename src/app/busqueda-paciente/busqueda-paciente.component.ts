9047ab697978b1a7667a544816e803c3f08b615e
CynthiaOrtiz <cynthia.ortiz.ceol@gmail.com>
2021-02-21 12:29:53 -0500

    3 files modified

--- src/app/busqueda-paciente/busqueda-paciente.component.ts
+++ src/app/busqueda-paciente/busqueda-paciente.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { Paciente } from "../paciente";
 
 @Component({
   selector: "app-busqueda-paciente",
@@ -7,10 +8,19 @@ import { Router } from "@angular/router";
   styleUrls: ["./busqueda-paciente.component.css"]
 })
 export class BusquedaPacienteComponent implements OnInit {
+  paciente = new Paciente(null, "", "", "", null);
+
   constructor(private router: Router) {}
 
   ngOnInit() {}
 
+  buscarPaciente() {
+    console.log("buscar paciente", this.paciente);
+  }
+
+  limpiarFiltros() {
+    console.log("limpiar filtros", this.paciente);
+  }
   /**newPaciente() {
     this.router.navigate([
       "/busqueda",
