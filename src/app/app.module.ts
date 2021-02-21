d2121c9658ff54109cf3a17300d28f9fe3b1edb4
CynthiaOrtiz <cynthia.ortiz.ceol@gmail.com>
2021-02-21 12:06:21 -0500

    7 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { PacienteFormComponent } from "./paciente-form/paciente-form.component";
-
 import { BusquedaPacienteComponent } from "./busqueda-paciente/busqueda-paciente.component";
-
 import { AppRoutingModule } from "./app-routing.module";
 
+import { CommonModule } from "@angular/common";
+import { MatIconModule, MatButtonModule } from "@angular/material";
+import {
+  FontAwesomeModule,
+  FaIconLibrary
+} from "@fortawesome/angular-fontawesome";
+
+import { fas, faStar, faFilm } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    FontAwesomeModule
+  ],
   declarations: [
     AppComponent,
     PacienteFormComponent,
@@ -19,7 +35,11 @@ import { AppRoutingModule } from "./app-routing.module";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(fas, far, faStar);
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
