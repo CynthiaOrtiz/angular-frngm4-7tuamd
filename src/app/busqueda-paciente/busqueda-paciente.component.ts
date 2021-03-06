5ccd3a05dde0123b4054a06c16faacb12580abf4
CynthiaOrtiz <cynthia.ortiz.ceol@gmail.com>
2021-03-05 19:45:09 -0500

    3 files modified

--- src/app/busqueda-paciente/busqueda-paciente.component.ts
+++ src/app/busqueda-paciente/busqueda-paciente.component.ts
@@ -12,18 +12,48 @@ export class BusquedaPacienteComponent implements OnInit {
   paciente = new Paciente(null, "", "", "", null, "", "", "", "");
   rowData: any[];
   columnDefs = [
-    { field: "identificador", sortable: true, filter: true },
-    { field: "nombre", sortable: true, filter: true },
-    { field: "apellido", sortable: true, filter: true },
-    { field: "direccion", sortable: true, filter: true },
-    { field: "telefono", sortable: true, filter: true },
-    { field: "direccion", sortable: true, filter: true },
-    { field: "ocupacion", sortable: true, filter: true },
-    { field: "genero", sortable: true, filter: true },
-    { field: "mail", sortable: true, filter: true },
-    { field: "nacimiento", sortable: true, filter: true }
+    {
+      headerName: "cedula",
+      field: "identificador",
+      sortable: true,
+      filter: true
+    },
+    { headerName: "nombre", field: "nombre", sortable: true, filter: true },
+    { headerName: "apellido", field: "apellido", sortable: true, filter: true },
+    {
+      headerName: "direccion",
+      field: "direccion",
+      sortable: true,
+      filter: true
+    },
+    { headerName: "telefono", field: "telefono", sortable: true, filter: true },
+    {
+      headerName: "ocupacion",
+      field: "ocupacion",
+      sortable: true,
+      filter: true
+    },
+    { headerName: "genero", field: "genero", sortable: true, filter: true },
+    { headerName: "mail", field: "mail", sortable: true, filter: true },
+    {
+      headerName: "nacimiento",
+      field: "nacimiento",
+      sortable: true,
+      filter: true
+    }
   ];
-  constructor(private router: Router, private http: HttpClient) {}
+  constructor(private router: Router, private http: HttpClient) {
+    this.rowData = [
+      {
+        identificador: 5,
+        nombre: 10,
+        apellido: 10,
+        direccion: 15,
+        telefono: 15,
+        ocupacion: 20
+      }
+    ];
+  }
 
   ngOnInit() {
     //this.rowData = this.http.get(
