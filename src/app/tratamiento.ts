720c5e2e6e57fde179a98fa5124c7a670585980e
CynthiaOrtiz <cynthia.ortiz.ceol@gmail.com>
2021-03-30 20:15:28 -0500

    tratamientos 1 file modified, 1 added

---
+++ src/app/tratamiento.ts
@@ -0,0 +1,10 @@
+export class tratamiento {
+  constructor(
+    public descripcion: string,
+    public observacion: string,
+    public tipoTratamiento: string,
+    public zonas: string,
+    public fecha: string,
+    public antecedente: string
+  ) {}
+}
