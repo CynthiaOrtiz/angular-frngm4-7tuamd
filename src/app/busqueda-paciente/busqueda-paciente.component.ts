830e51f95b08290c0e75fae8e0452b5ae970460d
CynthiaOrtiz <cynthia.ortiz.ceol@gmail.com>
2021-05-17 17:57:49 -0500

    1 file modified

--- src/app/busqueda-paciente/busqueda-paciente.component.ts
+++ src/app/busqueda-paciente/busqueda-paciente.component.ts
@@ -1,16 +1,16 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { Paciente } from "../paciente";
-import { HttpClient } from "@angular/common/http";
-import { DataService } from "../data.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Paciente } from '../paciente';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from '../data.service';
 
 @Component({
-  selector: "app-busqueda-paciente",
-  templateUrl: "./busqueda-paciente.component.html",
-  styleUrls: ["./busqueda-paciente.component.css"]
+  selector: 'app-busqueda-paciente',
+  templateUrl: './busqueda-paciente.component.html',
+  styleUrls: ['./busqueda-paciente.component.css']
 })
 export class BusquedaPacienteComponent implements OnInit {
-  paciente = new Paciente(null, "", "", "", null, "", "", "", "");
+  paciente = new Paciente(null, '', '', '', null, '', '', '', '');
 
   public pacientes = [];
 
@@ -21,53 +21,54 @@ export class BusquedaPacienteComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    console.log(
-      "buscar pacientes TODOS: ",
-      this.http.get("https://localhost:8080/example-assets/small-row-data.json")
-    );
+    this.http
+      .get('https://localhost:8080/example-assets/small-row-data.json')
+      .subscribe(data => {
+        console.log('buscar pacientes TODOS: ', data);
+      });
   }
 
   buscarPaciente() {
     let paciente = new Paciente(
       111111111111,
-      "asidhaosudhais",
-      "sdfghsdfgfgh",
-      "sdfgdfgdfg",
+      'asidhaosudhais',
+      'sdfghsdfgfgh',
+      'sdfgdfgdfg',
       654654,
-      "sdfgsdfg",
-      "sdfgsdfgdfg",
-      "sdfgdfgsd",
-      "dfgsdfgsdf"
+      'sdfgsdfg',
+      'sdfgsdfgdfg',
+      'sdfgdfgsd',
+      'dfgsdfgsdf'
     );
     let paciente2 = new Paciente(
       222222,
-      "dddddd",
-      "ddddd",
-      "dddd",
+      'dddddd',
+      'ddddd',
+      'dddd',
       654654,
-      "ddd",
-      "ddd",
-      "email",
-      "nacime"
+      'ddd',
+      'ddd',
+      'email',
+      'nacime'
     );
     this.pacientes.push(paciente);
     this.pacientes.push(paciente2);
-    console.log("buscar paciente", this.pacientes);
+    console.log('buscar paciente', this.pacientes);
     console.log(
-      "buscar pacientes TODOS1: ",
+      'buscar pacientes TODOS1: ',
       this.http.get(
-        "localhost:8080/adminPacServ/api/public/adminpaciente/buscarPacientes"
+        'localhost:8080/adminPacServ/api/public/adminpaciente/buscarPacientes'
       )
     );
   }
 
   limpiarFiltros() {
-    console.log("limpiar filtros", this.paciente);
+    console.log('limpiar filtros', this.paciente);
   }
 
   editarPaciente(pacienteEdit) {
-    console.log("editar ", pacienteEdit);
-    this.router.navigate(["paciente-form"], {
+    console.log('editar ', pacienteEdit);
+    this.router.navigate(['paciente-form'], {
       queryParams: { nombre: pacienteEdit.nombre, paciente: pacienteEdit }
     });
 
